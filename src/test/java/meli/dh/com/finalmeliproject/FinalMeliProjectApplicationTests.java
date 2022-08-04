7712a785af83874aea0e4cdf9239b5690ad10651
lauraagra-meli <laura.agra@mercadolivre.com>
2022-08-04 16:46:26 -0300

    starting requirement 1

---
+++ src/test/java/meli/dh/com/finalmeliproject/FinalMeliProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package meli.dh.com.finalmeliproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalMeliProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
