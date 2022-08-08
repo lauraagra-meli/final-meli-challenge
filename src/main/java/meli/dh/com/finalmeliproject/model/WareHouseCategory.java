0642f277a0dd91cd3262d771837d1042e79e339f
lauraagra-meli <laura.agra@mercadolivre.com>
2022-08-08 20:01:31 -0300

    feat-01: testing WareHouseCategory class

--- src/main/java/meli/dh/com/finalmeliproject/model/WareHouseCategory.java
+++ src/main/java/meli/dh/com/finalmeliproject/model/WareHouseCategory.java
@@ -1,12 +1,16 @@
 package meli.dh.com.finalmeliproject.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class WareHouseCategory {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
