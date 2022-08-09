4e8965e41935aec58cf7bb2bf1ee117c1cad2d94
Amanda Gonçalves <amanda.pgoncalves@mercadolivre.com>
2022-08-09 09:27:14 -0300

    feat/01: Representative Service test - part1

---
+++ src/test/java/meli/dh/com/finalmeliproject/mocks/GenerateRepresentative.java
@@ -0,0 +1,11 @@
+package meli.dh.com.finalmeliproject.util;
+
+import meli.dh.com.finalmeliproject.model.Representative;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenerateRepresentative {
+
+
+}
--- src/test/java/meli/dh/com/finalmeliproject/service/Representative/RepresentativeServiceTest.java
+++ src/test/java/meli/dh/com/finalmeliproject/service/Representative/RepresentativeServiceTest.java
@@ -1,12 +1,27 @@
 package meli.dh.com.finalmeliproject.service.Representative;
 
+import meli.dh.com.finalmeliproject.repository.IRepresentativeRepo;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
+
 class RepresentativeServiceTest {
+
+    @InjectMocks
+    private RepresentativeService representativeService;
+
+    @Mock
+    private IRepresentativeRepo representativeRepo;
 
     @Test
     void findRepresentativeById() {
+        BDDMockito.when(representativeRepo.findById())
     }
 }
