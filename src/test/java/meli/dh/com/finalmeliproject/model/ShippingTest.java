bef3f99d4f981c538315427c47b53d6629371129
saviopagung <savio.pagung@mercadolivre.com>
2022-08-10 15:39:21 -0300

    feat-02: tests shipping

--- src/test/java/meli/dh/com/finalmeliproject/model/ShippingTest.java
+++ src/test/java/meli/dh/com/finalmeliproject/model/ShippingTest.java
@@ -2,31 +2,56 @@ package meli.dh.com.finalmeliproject.model;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ShippingTest {
 
     @Test
-    void getId() {
+    void getAndSetId() {
+        Shipping s = new Shipping();
+        s.setId(1);
+
+        assertThat(s.getId()).isEqualTo(1);
     }
 
     @Test
-    void getShippingCapacity() {
+    void getAndSetShippingCapacity() {
+        Shipping s = new Shipping();
+        s.setShippingCapacity(1);
+
+        assertThat(s.getShippingCapacity()).isEqualTo(1);
     }
 
     @Test
-    void getShippingCategoryList() {
+    void getAndSetShippingCategoryList() {
+        Shipping s = new Shipping();
+        List<ShippingCategory> categories = new ArrayList<>();
+        s.setShippingCategoryList(categories);
+
+        assertThat(s.getShippingCategoryList()).isEqualTo(categories);
     }
 
     @Test
-    void setId() {
+    void allConstructor() {
+        List<ShippingCategory> categories = new ArrayList<>();
+        Shipping s = new Shipping(
+                1,
+                1,
+                categories
+        );
+
+        assertThat(s).isNotNull();
     }
 
     @Test
-    void setShippingCapacity() {
+    void builder() {
+        Shipping s = Shipping.builder().build();
+
+        assertThat(s).isNotNull();
     }
 
-    @Test
-    void setShippingCategoryList() {
-    }
 }
