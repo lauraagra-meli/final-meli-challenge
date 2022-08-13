d3cfd5e7b8d6e1040b28c32b6a3394a9d6621c8f
lauraagra-meli <laura.agra@mercadolivre.com>
2022-08-12 22:57:50 -0300

    final feature 2

--- src/main/java/meli/dh/com/finalmeliproject/service/shoppingCart/ShoppingCartService.java
+++ src/main/java/meli/dh/com/finalmeliproject/service/shoppingCart/ShoppingCartService.java
@@ -14,8 +14,6 @@ import meli.dh.com.finalmeliproject.service.product.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
