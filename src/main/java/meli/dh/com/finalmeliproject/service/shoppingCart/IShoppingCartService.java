60bc9932b460430fd510866b575bf3a05a9d6b2d
lauraagra-meli <laura.agra@mercadolivre.com>
2022-08-16 11:30:42 -0300

    init feature 6

--- src/main/java/meli/dh/com/finalmeliproject/service/shoppingCart/IShoppingCartService.java
+++ src/main/java/meli/dh/com/finalmeliproject/service/shoppingCart/IShoppingCartService.java
@@ -2,6 +2,7 @@ package meli.dh.com.finalmeliproject.service.shoppingCart;
 
 import meli.dh.com.finalmeliproject.dto.shoppingCart.PurchaseOrderDto;
 import meli.dh.com.finalmeliproject.dto.shoppingCart.ResponseShoppingCartDto;
+import meli.dh.com.finalmeliproject.model.ProductShoppingCart;
 import meli.dh.com.finalmeliproject.model.PurchaseOrder;
 import meli.dh.com.finalmeliproject.model.ShoppingCart;
 
@@ -12,4 +13,5 @@ public interface IShoppingCartService {
     ResponseShoppingCartDto shoppingCart(PurchaseOrderDto dto);
     ShoppingCart findShoppingCartProductsById(long id);
     PurchaseOrder editShoppingCart(long id);
+    ProductShoppingCart editProductShoppingCart(long id);
 }
